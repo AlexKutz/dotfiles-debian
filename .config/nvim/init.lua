b6f2aa399ff6e72876d7ebf747626b3bacac7afe
Alex <kurtzalexo@gmail.com>
2025-08-22 23:45:34 +0300

    nvim: add colorscheme preview

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,7 @@ require("config.lazy")
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+local map = vim.api.nvim_set_keymap
+
+map("n", "<C-j>", "<Esc>:FzfLua colorschemes<CR>", {})
